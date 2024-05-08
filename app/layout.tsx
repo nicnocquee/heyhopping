76ff79ac253ed5e01931eaf9669a267e68bff36f
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-08 22:10:48 +0200

    init

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import { GeistSans } from 'geist/font/sans'
+import './globals.css'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { CSPostHogProvider } from '@/app/analytics'
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Next.js and Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <CSPostHogProvider>
+        <TooltipProvider>
+          <body className="bg-background text-foreground">
+            <main className="flex min-h-screen flex-col items-center">{children}</main>
+          </body>
+        </TooltipProvider>
+      </CSPostHogProvider>
+    </html>
+  )
+}
