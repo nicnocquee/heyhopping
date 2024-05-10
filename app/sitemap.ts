10a0fc964c912a62a8561344d65116fc85feefb7
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-10 23:12:37 +0200

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,54 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { MetadataRoute } from 'next'
 import { generateStaticParams } from './[country]/[lang]/[city]/page'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const pages = ['https://www.heyhopping.com']
+  const pages = [
+    {
+      url: 'https://www.heyhopping.com',
+    },
+  ]
 
   const countryLangCityPages = await generateStaticParams()
 
-  pages.push(
-    ...countryLangCityPages.map(({ country, lang, city }) => {
-      return `https://www.heyhopping.com/${country}/${lang}/${city}`
-    })
-  )
+  const grouped = countryLangCityPages.reduce((acc, cur) => {
+    const country = cur.country
+    const lang = cur.lang
+    const city = cur.city
+    const identifier = `${country}/${city}`
+    if (!acc[identifier]) {
+      acc[identifier] = {}
+    }
+    if (lang === 'en') {
+      acc[identifier].url = `https://www.heyhopping.com/${country}/en/${city}`
+    } else {
+      if (!acc[identifier].alternates) {
+        acc[identifier].alternates = {
+          languages: {},
+        }
+      }
 
-  return pages.map((url) => ({
-    url,
-    lastModified: new Date(),
-    // alternates: {
-    //   languages: {
-    //     es: 'https://www.heyhopping.com/es',
-    //     de: 'https://www.heyhopping.com/de',
-    //   },
-    // },
-    changefreq: 'daily',
-    priority: 1,
-  }))
+      acc[identifier].alternates.languages[lang] =
+        `https://www.heyhopping.com/${country}/${lang}/${city}`
+    }
+    return acc
+  }, {} as any)
+
+  console.log(grouped)
+
+  Object.keys(grouped).forEach((key) => {
+    const item = grouped[key]
+    pages.push(item)
+  })
+
+  const data = pages.map((page) => {
+    return {
+      ...page,
+      changeFrequency: 'daily' as const,
+      priority: 1,
+      lastModified: new Date(),
+    }
+  })
+
+  return data
 }
