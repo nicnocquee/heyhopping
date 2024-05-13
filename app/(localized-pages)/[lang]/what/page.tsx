106b556e2cccd693ccc1d8be17d33adba5f5b254
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-13 22:54:19 +0200

    remove unnecessary code

--- app/(localized-pages)/[lang]/what/page.tsx
+++
@@ -1,6 +0,0 @@
-import { SupportedLanguage } from '@/locales/.generated/server'
-import WhatClient from './what'
-
-export default function WhatPage({ params: { lang } }: { params: { lang: SupportedLanguage } }) {
-  return <WhatClient lang={lang} />
-}
